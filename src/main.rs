4ceea1e79426f201795b5fc86484fda135c283a9
Jedjoud10 <jribijed6@gmail.com>
2024-04-30 20:34:57 -0400

    Damn it works

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,45 @@
 use std::{io::Cursor, path::Path};
+use rand::seq::SliceRandom;
 use image::io::Reader as ImageReader;
 mod network;
 pub use network::*;
 
 fn main() {
+    let (mut tx, mut rx) = std::sync::mpsc::channel::<Data>();
     
-    let mut rng = rand::thread_rng();
-    let mut datas = Vec::<Data>::new();
-    
-    for k in 0..4 {
-        let color = ["blue", "green", "red", "yellow"][k];
-        for i in 1..105 {
-            let img = ImageReader::open(format!("./src/images/{color}{i}.jpg")).unwrap().decode().unwrap();
-            let img = img.resize_exact(8, 8, image::imageops::FilterType::Gaussian);
-            let img = img.into_rgb32f();
-            let input = img.to_vec();
-            dbg!(input.len());
-            let mut output = vec![0.0f32; 4];
-            output[k] = 1.0;
-
-            datas.push(Data {
-                inputs: input,
-                outputs: output,
-            });
+    rayon::scope(|s| {
+        for k in 0..4 {
+            let color = ["blue", "green", "red", "yellow"][k];
+            for i in 1..105 {
+                let tx = tx.clone();
+                s.spawn(move |_| {
+                    let path = format!("./src/images/{color}{i}.jpg");
+                    println!("Loading image {path}");
+                    let img = ImageReader::open(path).unwrap().decode().unwrap();
+                    let img = img.resize_exact(8, 8, image::imageops::FilterType::Gaussian);
+                    let img = img.into_rgb32f();
+                    let input = img.to_vec();
+                    let mut output = vec![0.0f32; 4];
+                    output[k] = 1.0;
+        
+                    tx.send(Data {
+                        inputs: input,
+                        outputs: output,
+                    }).unwrap();
+                });
+                
+            }
         }
-    }
+    });
+    
 
     let mut nn = Network::input(8*8*3)
-        .dense(32)
+        .dense(8)
         .output(4);
 
-    nn.find_lowest_cost_nn(&datas, 10000, 10, 0.02, &mut rng);
-    nn.test_all(&datas);
-
-    /*
-
-    // how many input -> output lines
-    let data_count = 4;
-    let input_shape = 3;
-
-    // layers:
-    // input: 2 neurons
-    // output: 1
-    for _ in 0..10 {
-        
-        for _ in 0..100 {
-            let mut bias = vec![0.0; 3];
-            let mut factor = vec![0.0; 3];
-    
-            for i in 0..input_shape {
-                bias[i] = rng.gen_range(-1.0..1.0);
-                factor[i] = rng.gen_range(-1.0..1.0);
-            }
-    
-            let mut total = 0.0f32;
-            for i in 0..4 {
-                let input = &inputs[(input_shape * i)..(input_shape * (i+1))];
-                let output = outputs[i];
-    
-                let sum = (0..3).into_iter()
-                    .map(|i| activation(bias[i] + factor[i] * input[i]))
-                    .sum::<f32>();
-    
-                total += (output - sum).abs();
-            }    
-            dbg!(total);
-    
-            if total <= 1.0 {
-                dbg!(bias);
-                dbg!(factor);
-            }
-        }
-    }
-    */
-    
+    let mut rng = rand::thread_rng();
+    let mut datas = rx.try_iter().collect::<Vec<_>>();
+    datas.shuffle(&mut rng);
+    nn.find_lowest_cost_nn(&datas, 0.1, 10, 0.02);
+    nn.test_all(&datas);    
 }
--- src/network.rs
+++ src/network.rs
@@ -1,9 +1,10 @@
-use rand::{rngs::ThreadRng, Rng};
+use rand::{rngs::ThreadRng, seq::SliceRandom, Rng};
 use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 fn activation(val: f32) -> f32 {
-    val.max(0.0)
+    //val.max(0.0)
     //val.clamp(0.0, 1.0)
+    1.0f32 / (1.0f32 + std::f32::consts::E.powf(-val))
 }
 
 pub struct Data {
@@ -85,12 +86,7 @@ impl Network {
                 neuron.bias + neuron.weights.par_iter().enumerate().map(|(i, &weight)| last_layer_activations[i] * weight).sum::<f32>()
             });
 
-            let temp_next = if i == 1 {
-                temp_next.collect::<Vec<_>>()  
-            } else {
-                temp_next.map(activation).collect::<Vec<_>>()
-            };
-
+            let temp_next = temp_next.map(activation).collect::<Vec<_>>();
             last_layer_activations = temp_next;
         }
 
@@ -105,35 +101,40 @@ impl Network {
     }
 
     pub fn cost_all(&self, datas: &[Data]) -> f32 {
+        let datas = &datas[0..20];
         return datas.par_iter().map(|x| self.cost(x)).sum::<f32>() / (datas.len() as f32);
     }
 
     pub fn test_all(&self, datas: &[Data]) {
         for data in datas {
-            dbg!(&data.outputs);
-            dbg!(self.eval(data));
+            let predicted = self.eval(data).iter().enumerate().max_by(|a, b| (a.1).total_cmp(b.1)).unwrap().0;
+            let real = &data.outputs.iter().enumerate().max_by(|a, b| (a.1).total_cmp(b.1)).unwrap().0;
+            
+            println!("Real: {real}, predict: {predicted}. {}", *real == predicted);
         }
     }
 
-    pub fn find_lowest_cost_nn(&mut self, datas: &[Data], generations: usize, entities: usize, randomized: f32, rng: &mut ThreadRng) {
+    pub fn find_lowest_cost_nn(&mut self, datas: &[Data], threshold: f32, entities: usize, randomized: f32) {
         let mut saved = self.save();
-        let mut delta = vec![0.0f32; saved.len()];
         let mut min = f32::MAX;
-        while min > 0.1 {
+        let mut diff = 1.0;
+        let mut rng = rand::thread_rng();
+        while min > threshold {
             for k in 0..entities {
                 let mut nn = self.clone();
-                let a = saved.iter().zip(delta.iter()).map(|(a, b)| *a + b * 0.0).collect::<Vec<_>>();
-                nn.load(a);
-                nn.randomize(rng, randomized);
+                //let a = saved.iter().zip(delta.iter()).map(|(a, b)| *a + b * -0.2).collect::<Vec<_>>();
+                nn.load(saved.clone());
+                nn.randomize(&mut rng, randomized * (diff + 0.1));
                 
                 let score = nn.cost_all(&datas);
-                println!("Entity: {k}, score: {score}");
+                //println!("Score: {score}");
             
                 if score < min {
                     let temp = nn.save(); 
-                    delta = temp.iter().zip(saved).map(|(a, b)| b - a).collect::<Vec<_>>();
                     saved = temp;
                     min = score;
+                    //diff = (min - threshold).abs() / ((min + threshold) / 2.0);
+                    diff = (min - threshold).abs() * 10.0;
                     println!("Best Score: {min}");
                 }
             }
